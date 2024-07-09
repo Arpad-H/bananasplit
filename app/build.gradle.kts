4342cd644950d7686aa77bf556a5a766c427b6c4
ahorvath <arpad.horvath@hs-osnabrueck.de>
2024-07-10 00:16:14 +0200

    text recognition works. TODO: asjust regex

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,9 @@ dependencies {
     implementation(libs.camera.view)
     implementation(libs.play.services.mlkit.text.recognition)
     implementation(libs.gson)
+    implementation(libs.stanford.corenlp)
+//    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar", "*.jar"), "exclude" to listOf(""))))
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
